e64e14c6253e9e3ea3340a843fc7c70bb2a1ee53
oc <oc@yunify.com>
2018-11-20 15:45:43 +0800

    server: fix handleRouteRefresh and softResetOut
    both assume that getBestFromLocal() returns filtered paths by policies
    but it doesn't. Paths filtered by policies are returned as withdraw
    paths.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>

--- server/server.go
+++ server/server.go
@@ -851,11 +851,7 @@ func (s *BgpServer) handleRouteRefresh(peer *Peer, e *FsmMsg) []*table.Path {
 		return nil
 	}
 	rfList := []bgp.RouteFamily{rf}
-	accepted, filtered := s.getBestFromLocal(peer, rfList)
-	for _, path := range filtered {
-		path.IsWithdraw = true
-		accepted = append(accepted, path)
-	}
+	accepted, _ := s.getBestFromLocal(peer, rfList)
 	return accepted
 }
 
@@ -1841,16 +1837,20 @@ func (s *BgpServer) softResetOut(addr string, family bgp.RouteFamily, deferral b
 			}
 		}
 
-		pathList, filtered := s.getBestFromLocal(peer, families)
+		pathList, _ := s.getBestFromLocal(peer, families)
 		if len(pathList) > 0 {
-			sendFsmOutgoingMsg(peer, pathList, nil, false)
-		}
-		if !deferral && len(filtered) > 0 {
-			withdrawnList := make([]*table.Path, 0, len(filtered))
-			for _, p := range filtered {
-				withdrawnList = append(withdrawnList, p.Clone(true))
+			if deferral {
+				pathList = func() []*table.Path {
+					l := make([]*table.Path, 0, len(pathList))
+					for _, p := range pathList {
+						if !p.IsWithdraw {
+							l = append(l, p)
+						}
+					}
+					return l
+				}()
 			}
-			sendFsmOutgoingMsg(peer, withdrawnList, nil, false)
+			sendFsmOutgoingMsg(peer, pathList, nil, false)
 		}
 	}
 	return nil
