cd8e111890afb1bb179b600eb3a6e12c7ba9a2c1
jack <jack@yunify.com>
2019-01-18 15:06:37 +0800

    add version

--- gobgpd/main.go
+++ gobgpd/main.go
@@ -41,7 +41,7 @@ import (
 	"github.com/osrg/gobgp/table"
 )
 
-var version = "1.33.12"
+var version = "1.33.13"
 
 func marshalRouteTargets(l []string) ([]*any.Any, error) {
 	rtList := make([]*any.Any, 0, len(l))
